6c79a5f2d9bac27239a163b954d13be9a2673a63
Théo <theo.vincent@eleves.enpc.fr>
2021-11-17 09:42:35 +0000

    Image cropper

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name="birdClassification",
     version="0.1",
     description="Classifies bird from the Caltech-UCSD Birds-200-2011 dataset.",
-    packages=["classifier"],
+    packages=["classifier", "segmentor"],
     requires=["setuptools", "wheel"],
-    install_requires=["torch", "torchvision", "tqdm", "detectron2==0.6+cu102", "opencv-python"],
+    install_requires=["torch", "torchvision", "tqdm", "detectron2==0.6+cu102", "opencv-python", "pandas"],
     extras_require={
         "dev": ["tqdm", "ipykernel", "black"],
     },
@@ -14,6 +14,7 @@ setup(
         "console_scripts": [
             "train=classifier.train:train_cli",
             "generate_submission=classifier.generate_submission:generate_submission_cli",
+            "segment_from_label=segmentor.segment_from_label:segment_from_label_cli",
         ]
     },
 )
