3d49935b46cfe832e1e4d8e5080472c36fc4c1cb
Théo <theo.vincent@eleves.enpc.fr>
2021-11-21 21:25:12 +0000

    Try training with 4 epochs

--- scripts/run_training_4D.sh
+++ scripts/run_training_4D.sh
@@ -2,7 +2,7 @@
 
 for MODEL in resnet alexnet vgg squeezenet densenet
 do
-    train -c -m $MODEL -pd segmentation_from_gt -4D -bs 32 -ne 80 -lr 0.0005 -po segmentation_from_gt
+    train -c -m $MODEL -pd segmentation_from_gt -4D -bs 32 -ne 4 -lr 0.0005 -po segmentation_from_gt
 
     cp output/segmentation_from_gt/$MODEL.feather /content/Drive/MyDrive/MVA/ObjectRecognition/birdClassification/output/segmentation_from_gt/
     cp output/segmentation_from_gt/$MODEL\_$(get_best_model -m $MODEL -po segmentation_from_gt).pth /content/Drive/MyDrive/MVA/ObjectRecognition/birdClassification/output/segmentation_from_gt/
@@ -10,7 +10,7 @@ do
 done
 
 MODEL=efficientnet
-train -c -m $MODEL -pd segmentation_from_gt -4D -bs 8 -ne 80 -lr 0.00005 -po segmentation_from_gt
+train -c -m $MODEL -pd segmentation_from_gt -4D -bs 8 -ne 4 -lr 0.00005 -po segmentation_from_gt
 
 cp output/segmentation_from_gt/$MODEL.feather /content/Drive/MyDrive/MVA/ObjectRecognition/birdClassification/output/segmentation_from_gt/
 cp output/segmentation_from_gt/$MODEL\_$(get_best_model -m $MODEL -po segmentation_from_gt).pth /content/Drive/MyDrive/MVA/ObjectRecognition/birdClassification/output/segmentation_from_gt/
