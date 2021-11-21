4baad83faa54779c24e94d74cca25767f4d0d6df
Théo <theo.vincent@eleves.enpc.fr>
2021-11-21 22:28:57 +0000

    Reduce learning rate

--- scripts/run_training_4D.sh
+++ scripts/run_training_4D.sh
@@ -2,7 +2,7 @@
 
 for MODEL in resnet alexnet vgg squeezenet densenet
 do
-    train -c -m $MODEL -pd segmentation_from_gt -4D -bs 32 -ne 4 -lr 0.0005 -po segmentation_from_gt
+    train -c -m $MODEL -pd segmentation_from_gt -4D -bs 32 -ne 4 -lr 0.0001 -po segmentation_from_gt
 
     cp output/segmentation_from_gt/$MODEL.feather /content/Drive/MyDrive/MVA/ObjectRecognition/birdClassification/output/segmentation_from_gt/
     cp output/segmentation_from_gt/$MODEL\_$(get_best_model -m $MODEL -po segmentation_from_gt).pth /content/Drive/MyDrive/MVA/ObjectRecognition/birdClassification/output/segmentation_from_gt/
