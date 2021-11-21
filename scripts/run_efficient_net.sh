098a8be89e9593ac5614cbfeaa06e67fabdca247
Théo <theo.vincent@eleves.enpc.fr>
2021-11-21 11:17:04 +0000

    Reduce batch size efficient net

---
+++ scripts/run_efficient_net.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for MODEL in efficientnet
+do
+    train -c -m $MODEL -pd crop_from_gt -bs 16 -ne 80 -lr 0.0005 -po crop_from_gt
+
+    cp output/crop_from_gt/$MODEL.feather /content/Drive/MyDrive/MVA/ObjectRecognition/birdClassification/output/crop_from_gt/
+    cp output/crop_from_gt/$MODEL\_$(get_best_model -m $MODEL -po crop_from_gt).pth /content/Drive/MyDrive/MVA/ObjectRecognition/birdClassification/output/crop_from_gt/
+    rm output/crop_from_gt/$MODEL*
+done
