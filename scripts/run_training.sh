39f3fd1a4f05a8a9c60ba637cca9df7b07b2c212
Théo <theo.vincent@eleves.enpc.fr>
2021-11-21 10:15:00 +0000

    Reduce batch size

--- scripts/run_training.sh
+++ scripts/run_training.sh
@@ -2,7 +2,7 @@
 
 for MODEL in resnet alexnet vgg squeezenet densenet
 do
-    train -c -m $MODEL -pd crop_from_gt -bs 64 -ne 80 -lr 0.0005 -po crop_from_gt
+    train -c -m $MODEL -pd crop_from_gt -bs 32 -ne 80 -lr 0.0005 -po crop_from_gt
 
     cp output/crop_from_gt/$MODEL.feather /content/Drive/MyDrive/MVA/ObjectRecognition/birdClassification/output/crop_from_gt/
     cp output/crop_from_gt/$MODEL\_$(get_best_model -m $MODEL -po crop_from_gt).pth /content/Drive/MyDrive/MVA/ObjectRecognition/birdClassification/output/crop_from_gt/
