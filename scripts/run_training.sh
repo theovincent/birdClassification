c368349bb264b8ced66071588a813bd996c5f656
Théo <theo.vincent@eleves.enpc.fr>
2021-11-21 08:44:35 +0000

    Remove weights on colab to gain space

--- scripts/run_training.sh
+++ scripts/run_training.sh
@@ -6,4 +6,5 @@ do
 
     cp output/crop_from_gt/$MODEL.feather /content/Drive/MyDrive/MVA/ObjectRecognition/birdClassification/output/crop_from_gt/
     cp output/crop_from_gt/$MODEL\_$(get_best_model -m $MODEL -po crop_from_gt).pth /content/Drive/MyDrive/MVA/ObjectRecognition/birdClassification/output/crop_from_gt/
+    rm  output/crop_from_gt/$MODEL*
 done
