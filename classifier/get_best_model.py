e37ef4102b9f041e3b0e1ee6e8a15ba20706d55e
Théo <theo.vincent@eleves.enpc.fr>
2021-11-21 22:38:38 +0000

    Best way to select best model

--- classifier/get_best_model.py
+++ classifier/get_best_model.py
@@ -28,4 +28,25 @@ def get_best_model_cli(argvs=sys.argv[1:]):
 
     losses = pd.read_feather(f"output/{args.path_output}/{args.model}.feather").set_index("index")
 
-    print(int(losses.loc[losses.index % 2 == 1, "validation_loss"].idxmin()), end="")
+    odd_losses = losses.loc[losses.index % 2 == 1].copy()
+    # Get the rank of the epoch weights according to the validation loss
+    odd_losses["rank_loss"] = (
+        losses.loc[losses.index % 2 == 1]
+        .sort_values(by="validation_loss")
+        .reset_index()
+        .reset_index()
+        .set_index("index")["level_0"]
+    )
+    # Get the rank of the epoch weights according to the validation accuracy
+    odd_losses["rank_accuracy"] = (
+        losses.loc[losses.index % 2 == 1]
+        .sort_values(by="validation_accuracy", ascending=False)
+        .reset_index()
+        .reset_index()
+        .set_index("index")["level_0"]
+    )
+    odd_losses["rank_sum"] = odd_losses["rank_loss"] + odd_losses["rank_accuracy"]
+    odd_losses.sort_values(by="rank_sum").index[0]
+
+    # Take the best summed rank
+    print(int(odd_losses.sort_values(by="rank_sum").index[0]), end="")
