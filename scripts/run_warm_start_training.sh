4084c75af62a8e44e03a7e5e3973dde387fed20e
Théo <theo.vincent@eleves.enpc.fr>
2021-11-23 19:50:27 +0000

    Final tuning

--- scripts/run_warm_start_training.sh
+++ scripts/run_warm_start_training.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-train -c -m resnet -psw animals/resnet_$(get_best_model -m resnet -po animals).pth -pd segmentation_from_network -4D -bs 32 -ne 60 -lr 0.0005 -po segmentation_from_network_start_animals
+train -c -m resnet -psw animals/resnet_21.pth -pd segmentation_from_network -4D -bs 32 -ne 60 -lr 0.0005 -po segmentation_from_network_start_animals
 
 cp output/segmentation_from_network_start_animals/resnet.feather /content/Drive/MyDrive/MVA/ObjectRecognition/birdClassification/output/segmentation_from_network_start_gt/
 cp output/segmentation_from_network_start_animals/resnet_$(get_best_model -m resnet -po segmentation_from_network_start_animals).pth /content/Drive/MyDrive/MVA/ObjectRecognition/birdClassification/output/segmentation_from_network_start_animals/
 rm output/segmentation_from_network_start_animals/resnet*
 
 
-train -c -m densenet -psw animals/densenet_$(get_best_model -m densenet -po animals).pth -pd segmentation_from_network -4D -bs 32 -ne 60 -lr 0.0005 -po segmentation_from_network_start_animals
+train -c -m densenet -psw animals/densenet_19.pth -pd segmentation_from_network -4D -bs 32 -ne 60 -lr 0.0005 -po segmentation_from_network_start_animals
 
 cp output/segmentation_from_network_start_animals/densenet.feather /content/Drive/MyDrive/MVA/ObjectRecognition/birdClassification/output/segmentation_from_network_start_gt/
 cp output/segmentation_from_network_start_animals/densenet_$(get_best_model -m densenet -po segmentation_from_network_start_animals).pth /content/Drive/MyDrive/MVA/ObjectRecognition/birdClassification/output/segmentation_from_network_start_animals/
 rm output/segmentation_from_network_start_animals/densenet*
 
 
-train -c -m vgg -psw animals/vgg_$(get_best_model -m vgg -po animals).pth -pd segmentation_from_network -4D -bs 32 -ne 60 -lr 0.0005 -po segmentation_from_network_start_animals
+train -c -m vgg -psw animals/vgg_19.pth -pd segmentation_from_network -4D -bs 32 -ne 60 -lr 0.0005 -po segmentation_from_network_start_animals
 
 cp output/segmentation_from_network_start_animals/vgg.feather /content/Drive/MyDrive/MVA/ObjectRecognition/birdClassification/output/segmentation_from_network_start_gt/
 cp output/segmentation_from_network_start_animals/vgg_$(get_best_model -m vgg -po segmentation_from_network_start_animals).pth /content/Drive/MyDrive/MVA/ObjectRecognition/birdClassification/output/segmentation_from_network_start_animals/
